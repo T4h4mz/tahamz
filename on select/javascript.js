5b80276d8593bba7380c1d8a9f4ba4145177778f
Tahamz <tah.moradzadeh@gmail.com>
2021-03-13 22:30:46 +0330

    fix some problems
    icon updated
    objective javascript code
    move some attributes in index.html

--- on select/javascript.js
+++ on select/javascript.js
@@ -1,39 +1,43 @@
-var iconClassName = "fd-icon-1858152601";
+var fdWidget = function () {
+  var buttonClassName = "fd-button-1858152601";
 
-var showIcon = function () {
-  var img = document.createElement("img");
-  img.src = "icon.png";
-  img.classList.add(iconClassName);
-  s = window.getSelection().getRangeAt(0).getBoundingClientRect();
-  img.style.left = s.right + "px";
-  img.style.top = s.top - 15 + "px";
+  var showIcon = function () {
+    var img = document.getElementsByClassName(buttonClassName);
+    s = window.getSelection().getRangeAt(0).getBoundingClientRect();
+    img[0].style.left = s.right + "px";
+    img[0].style.top = s.top - 15 + "px";
+    img[0].style.display = "block";
+  };
 
-  document.body.appendChild(img);
+  var hideIcon = function () {
+    var img = document.getElementsByClassName(buttonClassName);
+
+    if (img.length <= 0) {
+      return;
+    }
+
+    img[0].style.display = "none";
+  };
+
+  var onChange = function () {
+    var selectedTextLength = window.getSelection().toString().length;
+    if (!selectedTextLength || selectedTextLength < 2) {
+      hideIcon();
+      return;
+    }
+
+    showIcon();
+  };
+
+  var buttonClick = function () {
+    alert("شما " + window.getSelection().toString() + " را انتخاب کردید.");
+  };
+
+  document.addEventListener("mouseup", onChange);
+
+  document
+    .getElementsByClassName("fd-button-1858152601")[0]
+    .addEventListener("click", buttonClick);
 };
 
-var hideIcon = function () {
-  var img = document.getElementsByClassName(iconClassName);
-
-  if (img.length <= 0) {
-    return;
-  }
-
-  document.body.removeChild(img[0]);
-};
-
-var onChange = function () {
-  var selectedTextLength = window.getSelection().toString().length;
-
-  if (!selectedTextLength || selectedTextLength < 2) {
-    hideIcon();
-    return;
-  }
-
-  if (document.getElementsByClassName(iconClassName).length > 0) {
-    return;
-  }
-
-  showIcon();
-};
-
-document.addEventListener("selectionchange", onChange);
+fdWidget();
